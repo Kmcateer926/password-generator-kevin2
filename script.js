d04cedd8e90966c6bb66c7d39173cd509863c59f
Kevin McAteer <kmcateer926@gmail.com>
2020-09-21 22:12:40 -0400

    added first prompt upon click

--- script.js
+++ script.js
@@ -9,25 +9,80 @@ var choices;
 
 //password variables as choices for password
 
-number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "?"]
-upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-lowerCase = ["b", "a", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "?"];
+upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+lowerCase = [
+  "b",
+  "a",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
 var generateBtn = document.querySelector("#generate");
 
 //ask for length
 //user input starts here
 function writePassword() {
-  
-  
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
+  passwordText.value = password;
 }
+
+function generatePassword() {
+  click = prompt("How many characters in password between 8 and 128.");
   
- 
+}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword)
-
-
-
+generateBtn.addEventListener("click", writePassword);
