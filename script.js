bd108affaec69fe1ee94af00ddbc3154826bfc2e
Kevin McAteer <kmcateer926@gmail.com>
2020-09-21 22:02:22 -0400

    added generate password to resolve error

--- script.js
+++ script.js
@@ -5,20 +5,22 @@ var confirmNumber;
 var confirmSpecialCharacter;
 var confirmUppercase;
 var confirmLowercase;
+var choices;
 
-//password variables
+//password variables as choices for password
 
 number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "?"]
 upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 lowerCase = ["b", "a", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var generateBtn = document.querySelector("#generate");
-//ask for length
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+var generateBtn = document.querySelector("#generate");
+
+//ask for length
+//user input starts here
+function writePassword() {
+  
+  
 
 }
   
