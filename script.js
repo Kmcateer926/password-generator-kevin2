a01d8b85656d8a1e2b0d59026dca4acc14d04f3f
Kevin McAteer <kmcateer926@gmail.com>
2020-09-21 21:52:22 -0400

    added upper and lower case variables

--- script.js
+++ script.js
@@ -2,14 +2,16 @@
 //user input to be set to confirms for true false statements
 var enter;
 var confirmNumber;
-var confirmCharacter;
+var confirmSpecialCharacter;
 var confirmUppercase;
 var confirmLowercase;
 
 //password variables
 
-number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0",]
-
+number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "?"]
+upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+lowerCase = ["b", "a", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var generateBtn = document.querySelector("#generate");
 //ask for length
 function writePassword() {
