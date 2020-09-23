9534b0922a76a82cf31a42f3c17a061a06dd4e5e
Kevin McAteer <kmcateer926@gmail.com>
2020-09-23 15:30:04 -0400

    changes variables from arrays to strings, added two new variables.  added 'include' to confirms

--- script.js
+++ script.js
@@ -1,72 +1,14 @@
 // Assignment Code
 //user input to be set to confirms for true false statements
 var enter;
-var confirmNumber;
-var confirmSpecialCharacter;
-var confirmUppercase;
-var confirmLowercase;
-var choices;
+var number = "0123456789";
+var specialCharacter = "?}{+_)(*&^%$#@!";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercase = "abcdefghijklmnopqrstuvwxyz"
+var choices = "";
+var password = "";
 
 //password variables as choices for password
-
-number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "?"];
-upperCase = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-lowerCase = [
-  "b",
-  "a",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
 
 var generateBtn = document.querySelector("#generate");
 
@@ -81,22 +23,26 @@ function writePassword() {
 //initial user input, from choosing length to whether they want numbers/characters/letters
 function generatePassword() {
 
- enter = parseInt(
-  prompt("How many characters in password between 8 and 128?")
+ enter = 
+  prompt("How many characters in password between 8 and 128?"
 );
 if (!enter) {
   alert("must enter value!");
 } else if (enter < 8 || enter > 128) {
   alert("must be between 8 and 128");
 } else {
-  confirmUppercase = confirm("will password contain Uppercase letters?");
-  confirmNumber = confirm("will password contain numbers?");
-  confirmLowercase = confirm("will password contain lowercase letters?");
-  confirmSpecialCharacter = confirm(
+  var includeUppercase = confirm("will password contain Uppercase letters?");
+  var includeNumber = confirm("will password contain numbers?");
+  var includeLowercase = confirm("will password contain lowercase letters?");
+  var includeSpecialCharacter = confirm(
     "will password contain special characters?"
   );
 }
-if (confirmUppercase && confirmNumber && confirmLowercase && confirmSpecialCharacter)
+
+if (includeUppercase) {
+  choices += uppercase;
+}
+if (uppercase && number && lowercase && specialCharacter)
 choices = specialCharacter.concat(number, upperCase, lowerCase)
 
 for (var i = 0; i < enter; i++) {
