aa4467b72d8d01bedc022864eb4568df87eb21b9
Kevin McAteer <kmcateer926@gmail.com>
2020-09-23 15:41:15 -0400

    finilized for statement

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 // Assignment Code
 //user input to be set to confirms for true false statements
+//password variables as choices for password
 var enter;
 var number = "0123456789";
 var specialCharacter = "?}{+_)(*&^%$#@!";
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowercase = "abcdefghijklmnopqrstuvwxyz"
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var choices = "";
 var password = "";
-
-//password variables as choices for password
-
 var generateBtn = document.querySelector("#generate");
 
 //ask for length
@@ -21,37 +19,46 @@ function writePassword() {
   passwordText.value = password;
 }
 //initial user input, from choosing length to whether they want numbers/characters/letters
+// user is alerted if it does not meet required criteria
 function generatePassword() {
+  enter = prompt("How many characters in password between 8 and 128?");
+  if (!enter) {
+    alert("must enter value!");
+  } else if (enter < 8 || enter > 128) {
+    alert("must be between 8 and 128");
+  } else {
+    var includeUppercase = confirm("will password contain Uppercase letters?");
+    var includeNumber = confirm("will password contain numbers?");
+    var includeLowercase = confirm("will password contain lowercase letters?");
+    var includeSpecialCharacter = confirm(
+      "will password contain special characters?"
+    );
+  }
 
- enter = 
-  prompt("How many characters in password between 8 and 128?"
-);
-if (!enter) {
-  alert("must enter value!");
-} else if (enter < 8 || enter > 128) {
-  alert("must be between 8 and 128");
-} else {
-  var includeUppercase = confirm("will password contain Uppercase letters?");
-  var includeNumber = confirm("will password contain numbers?");
-  var includeLowercase = confirm("will password contain lowercase letters?");
-  var includeSpecialCharacter = confirm(
-    "will password contain special characters?"
-  );
-}
+  if (includeUppercase) {
+    choices += uppercase;
+  }
+  if (includeLowercase) {
+    choices += lowercase;
+  }
+  if (includeNumber) {
+    choices += number;
+  }
+  if (includeSpecialCharacter) {
+    choices += specialCharacter;
+  }
 
-if (includeUppercase) {
-  choices += uppercase;
-}
-if (uppercase && number && lowercase && specialCharacter)
-choices = specialCharacter.concat(number, upperCase, lowerCase)
+  else if(!uppercase && !number && !lowercase && !specialCharacter)
+    alert("Must choose at least one set of criteria!")
 
-for (var i = 0; i < enter; i++) {
-  var pickChoices = choices[Math.floor(Math.random() * choices.length)];
-}
+
+  for (var i = 0; i < enter; i++) {
+    password += 
+     choices[Math.floor(Math.random() * choices.length)];
+  }
 
   return pickChoices;
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
